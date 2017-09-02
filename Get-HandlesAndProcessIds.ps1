b6be3c14a4799e22d91beaaf21fb4516d7306a17
Nate Schlutter <nate.schlutter@gmail.com>
2017-09-02 11:27:12 -0500

    Initial Commit

---
+++ Get-HandlesAndProcessIds.ps1
@@ -0,0 +1,72 @@
+﻿<#
+.Description
+Converts the text output of the SysInternals tool "handle.exe" into an object array
+ 
+.Example
+ 
+$handles = & handle.exe -a  -u -accepteula "MY_LOCKED_ASSEMBLY.dll"
+$allHandles = Get-HandlesAndProcessIds -HandleInfo $handles
+$allHandles
+ 
+---------------------------
+ 
+ProcessName : dllhost.exe
+ProcessId   : 6960
+Type        : File
+User        : DOMAIN\user
+HandleId    : B34
+Name        : MY_LOCKED_ASSEMBLY.dll
+AppPoolName : 
+ 
+#>
+
+Add-Type -Language CSharp @"
+public class Handle{
+    public string ProcessName { get;set;}
+    public int ProcessId {get;set;}
+    public string Type {get;set;}
+    public string User {get;set;}
+    public string HandleId { get;set;}
+    public string Name { get; set;}
+    public string AppPoolName {get;set;}
+}
+"@;
+ 
+function Get-HandlesAndProcessIds {
+    [OutputType([Handle[]])]
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [object[]]$HandleInfo)
+
+    $handle = new-object Handle
+    foreach ($line in $handles) {
+        $line=$Line.trim()
+        if($line -like "*pid:*") {
+            # $result = $line | Select-String –pattern '^([^ ]*)\s*pid: ([0-9]*)\s*type: ([^ ]*)\s*([^ ]*)\s*(.*?): (.*)'
+            $result = $line | Select-String –pattern '^(.*)\s*pid: ([0-9]*)\s*type: ([^ ]*)\s*([^ ]*)\s*(.*?): (.*)'
+
+            $handle.ProcessName =  $result.Matches[0].Groups[1].Value
+            $handle.ProcessId = [int]::Parse( $result.Matches[0].Groups[2].Value)
+            $handle.Type = $result.Matches[0].Groups[3].Value
+            $handle.User = $result.Matches[0].Groups[4].Value
+            $handle.HandleId =  $result.Matches[0].Groups[5].Value
+            $handle.Name = $result.Matches[0].Groups[6].Value
+ 
+            if($handle.ProcessName -like "w3wp.exe") {
+                $process = Get-AppPoolProcesses | where { $_.ProcessId -eq $handle.ProcessId }
+                if($process) {
+                    $handle.AppPoolName = $Process.AppPoolName
+                }
+            }
+ 
+            $handle
+        }
+    }
+}
+ 
+function Get-AppPoolProcesses {
+    Get-WmiObject -NameSpace 'root\WebAdministration' -class 'WorkerProcess' -ComputerName 'LocalHost' |
+        select AppPoolName, ProcessId |
+            sort -Property AppPoolName
+}
---
+++ handle.ps1
@@ -0,0 +1,13 @@
+﻿. "$PSScriptRoot\Get-HandlesAndProcessIds.ps1"
+
+$fileName = "c:\inetpub\ftproot\cdr\temp\"
+
+$handles = & handle.exe -a -u -accepteula "$fileName"
+$handleCount = (Get-HandlesAndProcessIds -HandleInfo $handles | Measure-Object)
+$openHandles = Get-HandlesAndProcessIds -HandleInfo $handles
+Write-Host "Count:" $handleCount.count
+$openHandles
+
+
+
+#$openHandles | Get-Member -MemberType Property
